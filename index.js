86089f77d00d4547600170909d6901e0f3ef6ba0
v1sh999 <vanshrajani2006@gmail.com>
2026-09-10 14:48:07 +0530

    displaySongs function created

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ pathToSongs = path.join(__dirname,'songs');
 
 let songs = fs.readdirSync(pathToSongs).filter((item)=> item.endsWith('.mp3'))
 
-songs.forEach(element => {
-    console.log(element)
-});
+function displaySongs(){
+    for (let i=0;i<songs.length;i++){
+        let song = songs[i].split('.')[0]
+        process.stdout.write(`${song}\n`)
+    }
+}
+displaySongs()
