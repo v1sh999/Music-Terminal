093b0ac49f5ac911fb33a7d4925853232fa0e999
v1sh999 <vanshrajani2006@gmail.com>
2026-09-12 19:38:50 +0530

    code cleaned up

--- index.js
+++ index.js
@@ -8,28 +8,36 @@ const setInput = process.stdin;
 const setOutput = process.stdout;
 
 // Variables
-setInput.setEncoding('utf-8');
-setInput.setRawMode(true);
-let songs = fs.readdirSync(pathToSongs).filter((item) => item.endsWith('.mp3'));
-let selected = 1;
 let audio;
 let currentSong;
+let selected = 1;
+
 
 async function init(){
+    FindSongs();
+    prepareTerminal();
     awaitAudioModule();
     WelcomeUser();
 };
 
+
+async function prepareTerminal(){
+    setInput.setEncoding('utf-8');
+    setInput.setRawMode(true);
+};
+async function FindSongs(){
+    let songs = fs.readdirSync(pathToSongs).filter((item) => item.endsWith('.mp3'));
+};
 async function awaitAudioModule(){
     let audioModule = await import('audio');
     audio = audioModule.default;
 };
-
 function WelcomeUser(){
     console.log(`🎶 Welcome to the Terminal Music Player 🎶\n`);
     displaySongs();
     console.log(`\n🎵 Select a number to play the song`);
 };
+
 
 function displaySongs(){
     setOutput.cursorTo(1,2);
@@ -45,14 +53,12 @@ function displaySongs(){
         };
     };
 };
-
 function quitApp(){
     setInput.setRawMode(false);
     setInput.pause();
     console.clear();
     process.exit(0);
 };
-
 function goUp(){
     if (selected === 1){
         selected = songs.length;
@@ -61,7 +67,6 @@ function goUp(){
     };
     displaySongs();
 };
-
 function goDown(){
     if (selected === songs.length){
         selected = 1;
@@ -71,6 +76,7 @@ function goDown(){
 
     displaySongs();
 };
+
 
 async function selectSong(){
     if (currentSong && currentSong.playing){
@@ -83,24 +89,28 @@ async function selectSong(){
     currentSong.play();
 };
 
+
+function handleInputs(input){
+    if (input === "q"){
+        quitApp();
+    };
+    if (input[2] === 'A'){
+        // When up arrow key is pressed
+        goUp();
+    };
+    if (input[2] === 'B'){
+        // When down arrow key is pressed
+        goDown();
+    };
+    if (input === '\r'){
+        selectSong();
+    };
+};
+
+
 async function main(){
     await init();
 
-    setInput.on('data',(input)=>{
-        if (input === "q"){
-            quitApp();
-        };
-        if (input[2] === 'A'){
-            // When up arrow key is pressed
-            goUp();
-        };
-        if (input[2] === 'B'){
-            // When down arrow key is pressed
-            goDown();
-        };
-        if (input === '\r'){
-            selectSong();
-        };
-    });
+    setInput.on('data',handleInputs);
 };
 main();
